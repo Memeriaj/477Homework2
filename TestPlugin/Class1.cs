751d1ed0e482675a5f7a4feafbeb0a5c3214f73d
Alex Memering <memeriaj@rose-hulman.edu>
2013-10-01 17:01:46 -0400

    Editted Test Plugin project
    Now has a window that should display Hello

--- TestPlugin/Class1.cs
+++ TestPlugin/Class1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PluginApp;
 
 namespace TestPlugin
 {
-    public class Class1
+    public class Class1 : AbstractPlugin
     {
+        public Class1()
+        {
+            this.Content2 = new UserControl1();
+        }
     }
 }
