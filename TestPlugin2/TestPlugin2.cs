d1cba6c37f2d2ad966a12d548f66e1f0561b9796
John MacAslan <macaslje@rose-hulman.edu>
2013-10-02 20:24:48 -0400

    report 1

---
+++ TestPlugin2/TestPlugin2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginApp;
+
+namespace TestPlugin2
+{
+    public class TestPlugin2 : AbstractPlugin
+    {
+        int counter = 0;
+
+        public TestPlugin2()
+        {
+            Action toPOST = new Action(() => this.OnStatusPosted(" has been relaxing in a sauna for " + counter++ + " minutes"));
+            this.Content2 = new UserControl1(toPOST);
+        }
+    }
+}
---
+++ TestPlugin2/UserControl1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using PluginApp;
+
+namespace TestPlugin2
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public Action PostAction {get; set;}
+
+        public UserControl1(Action post)
+        {
+            InitializeComponent();
+            PostAction = post;
+        }
+
+        private void postStatus(object sender, RoutedEventArgs e)
+        {
+            PostAction();
+        }
+    }
+}
